1c2d335cd26eadad5bd4f6cfc66f617aa18c3701
mehrshad9 <mehrshad9@gmail.com>
2020-11-01 23:30:12 -0500

    Rev2

--- materialpro.py
+++ materialpro.py
@@ -1,10 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
 
 
-class materialpro:
+
+
+
+# In[76]:
+
+
+import pandas as pd
+import requests
+import json
+class MaterialPro:    
     def __init__(self, Mat,key1="kI8OEewx6FTbDV4A"):
-        import requests
-        import json
-        import pandas as pd
         URL1="https://www.materialsproject.org/rest/v2/materials/"
         self.Mat=Mat
         self.key1=key1
@@ -34,8 +44,86 @@ class materialpro:
         dic1={}
         for op in self.options:
             dic1[op]=self.get_option(op)
-        return pd.DataFrame(dic1)
+        return pd.DataFrame(dic1)   
+    
         
+
+
+# In[ ]:
+
+
+
+
+
+# In[181]:
+
+
+class VaspInputs:
+    def __init__(self):
+        pass
+    def poscar_selec_dyn( self,a1,a2,a3,species_vec,basis_vec,lattice_cons=1,Comment ='Comment'):
+        fw=open('POSCAR','w')
+        fw.write(Comment)
+        fw.write('\n  {}'.format(float(lattice_cons)))
+        fw.write('\n  {} {} {}'.format(float(a1[0]),float(a1[1]),float(a1[2])))
+        fw.write('\n  {} {} {}'.format(float(a2[0]),float(a2[1]),float(a2[2])))
+        fw.write('\n  {} {} {}\n'.format(float(a3[0]),float(a3[1]),float(a3[2])))
+        fw.write(' '.join([str(x) for x in species_vec]))
+        fw.write('\n Selective dynamics \n Cartesian \n')
+        for b in basis_vec:
+            fw.write(' '.join([str(x) for x in b]))
+            fw.write('\n')
+        
+    def plot_bandgap(self):
+        pass
+    def bandgap(self):
+        pass
+    def poscar2unitcell(self):
+        pass
+    def unit_2_super(self,a1,a2,a3,basis_vec,nx = 1,ny = 1,nz = 1):
+        counter=0
+        nb=len(basis_vec)
+        coords = [[0 for col in range(3)] for row in range(nx*ny*nz*nb)]
+        for i in range(nx):
+            for j in range(ny):
+                for m in range(nz):
+                    for k in range(1,nb+1):                  
+                        counter+=1
+                        coords[counter]=[i*a[1][s]+j*a[2][s]+m*a[3][s]+b[k][s] for s in range(2)]
+            
+
+
+# In[ ]:
+
+
+
+
+
+# In[182]:
+
+
+a=VaspInputs()
+
+
+# In[183]:
+
+
+lv=[[0.5, 0.5, 0.5 ], [0.0, 0.0 ,0.0]]
+
+
+# In[184]:
+
+
+a.poscar_selec_dyn(a1=[0,1,1],a2=[1,0,1],a3=[1,1,0],species_vec=[1,1], lattice_cons=3.57, basis_vec= lv)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
 
 
 
--- materialpy.py
+++ materialpy.py
@@ -5,7 +5,7 @@ snse=materialpro('Fe')
 print(snse.Mat)
 df1=snse.get_df()
 print(df1.describe())
-
+print(snse.option)
 
 
 
